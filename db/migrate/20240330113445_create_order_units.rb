8a6a5e5ecb474efd5897f9ca89987e1de374adb5
underground0930 <go.nishiduka.1985@gmail.com>
2024-03-30 21:15:14 +0900

    スキーマを作成

--- db/migrate/20240330113445_create_order_units.rb
+++ db/migrate/20240330113445_create_order_units.rb
@@ -1,7 +1,7 @@
 class CreateOrderUnits < ActiveRecord::Migration[7.0]
   def change
     create_table :order_units do |t|
-
+      t.string :code, null: false, index: { unique: true }
       t.timestamps
     end
   end
--- db/migrate/20240330113926_create_order_details.rb
+++ db/migrate/20240330113926_create_order_details.rb
@@ -1,6 +1,8 @@
 class CreateOrderDetails < ActiveRecord::Migration[7.0]
   def change
     create_table :order_details do |t|
+      t.references :drink, null: false, foreign_key: true
+      t.references :order, null: false, foreign_key: true
 
       t.timestamps
     end
