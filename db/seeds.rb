be3cd593cab2801ff456a79a59016d0e27d33e7b
underground0930 <go.nishiduka.1985@gmail.com>
2024-03-30 21:24:06 +0900

    seedを

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# drink
+%w[ビール ハイボール ワイン カクテル ノンアルコール レモンサワー].each do |drink|
+  Drink.create!(name: drink, price: rand(300..800))
+end
+
+10.times do
+  OrderUnit.create!(code: SecureRandom.hex(16))
+end
