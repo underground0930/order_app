cceda308b071af69958a95d11a9af2ce02b3d872
underground0930 <go.nishiduka.1985@gmail.com>
2024-03-31 12:18:29 +0900

    モジュールオプションは要る

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   end
 
   resources :order_units, only: [:show], param: :code do
-    resources :orders, only: :create do
-      resource :complete, only: :create
+    resources :orders, only: :create, module: :order_units do
+      resource :complete, only: :create, module: :orders
     end
-    resources :order_histories, only: :index
+    resources :order_histories, only: :index, module: :order_units
   end
 end
+
