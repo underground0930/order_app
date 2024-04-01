f3681e5b44febd2a32c2780e8d2e1d36f7329bd3
underground0930 <go.nishiduka.1985@gmail.com>
2024-04-01 16:08:40 +0900

    qrコード生成

--- app/controllers/admin/order_units_controller.rb
+++ app/controllers/admin/order_units_controller.rb
@@ -1,8 +1,8 @@
 class Admin::OrderUnitsController < Admin::BaseController
   def index
+    @order_units = OrderUnit.all.order(created_at: :desc)
   end
 
   def create
   end
-
 end
--- app/models/order_unit.rb
+++ app/models/order_unit.rb
@@ -1,2 +1,17 @@
 class OrderUnit < ApplicationRecord
+  has_one :order, dependent: :restrict_with_error
+
+  validates :code, presence: true
+
+  def qrcode
+    qrcode = RQRCode::QRCode.new(Rails.application.routes.url_helpers.order_unit_path(self))
+    qrcode.as_svg(
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 6,
+      standalone: true,
+      use_path: true
+    ).html_safe
+  end
+
 end
